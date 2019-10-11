76a55846465215d398a2ab2757eb53722624af8f
Rohit Kumar <Rohit.Kumar@efi.com>
2019-10-11 11:45:02 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import * as firebase from 'firebase';
+
+//Copy the firebaseConfig from your firebase account after adding a web project
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  verificationId:any;
+  code: string = "";
+  phoneNumber: string = "";
   constructor() {}
+
+  send(){
+    this.phoneNumber = "+91"+this.phoneNumber;
+    (<any>window).FirebasePlugin.verifyPhoneNumber(this.phoneNumber, 60, (credential) =>{
+      alert("SMS sent successfully")
+      console.log(credential);
+  
+      this.verificationId = credential.verificationId;
+      
+      
+  }, (error)=> {
+      console.error(error);
+  });
+  }
+
+  verify(){
+    let signInCredential = firebase.auth.PhoneAuthProvider.credential(this.verificationId, this.code);
+    firebase.auth().signInWithCredential(signInCredential).then((info)=>{
+      console.log(info);
+      firebase.auth().currentUser.getIdToken().then((idToken)=>{
+        console.log(idToken)
+      })
+    },(error)=>{
+      console.log(error)
+    });
+  }
 
 }
